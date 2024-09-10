68e86d6c6903a43b9907a05985b0420d0d974cc8
Gustavo Gutiérrez <132943080+GAGV03@users.noreply.github.com>
2024-09-09 20:03:29 -0600

    Implemetación de componentes funcionales

--- Front/romulusweb/src/App/Pages/Page.jsx
+++ Front/romulusweb/src/App/Pages/Page.jsx
@@ -1,150 +1,250 @@
 import { useState } from "react";
-import {Box,FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput,Grid2} from "@mui/material";
-import {Visibility, VisibilityOff} from "@mui/icons-material";
+import {Box,FormControl,InputLabel, OutlinedInput,Slider,MenuItem,Select,FormControlLabel,Switch} from "@mui/material";
+import "../../Styles/page.css";
 
 export default function Formulario() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [showPassword, setShowPassword] = useState(false);
+    const [passengerID, setPassengerID] = useState("");
+    const [homePlanet, setHomePlanet] = useState("");
+    const [cryoSleep, setCryoSleep] = useState(false);
+    const [cabin, setCabin] = useState("");
+    const [destination, setDestination] = useState("");
+    const [age, setAge] = useState(0);
+    const [vip,setVip] = useState(false);
+    const [roomService, setRoomService] = useState(0);
+    const [foodCourt, setFoodCourt] = useState(0);
+    const [shoppingMall, setShoppingMall] = useState(0);
+    const [vrDeck, setVrDeck] = useState(0);
+    const [name, setName] = useState("");
+    const [transported, setTransported] = useState(false);
 
-    const handleShowPassword = () => {
-        setShowPassword(!showPassword);
+    const handlePassengerId = (event) => {
+        setPassengerID(event.target.value);
     };
-
-    const handleUsername = ( event) => {
-        setUsername(event.target.value);
-    } 
-
-    const handlePassword = ( event) =>{
-        setPassword(event.target.value);
-    }
-
+    
+    const handleHomePlanet = (event) => {
+        setHomePlanet(event.target.value);
+    };
+    
+    const handleCryoSleep = (event) => {
+        setCryoSleep(event.target.value);  // Asumimos que es un checkbox
+    };
+    
+    const handleCabin = (event) => {
+        setCabin(event.target.value);
+    };
+    
+    const handleDestination = (event) => {
+        setDestination(event.target.value);
+    };
+    
+    const handleAge = (event) => {
+        setAge(event.target.value);  // Convertimos el valor a número
+    };
+    
+    const handleVip = (event) => {
+        setVip(event.target.checked);  // Asumimos que es un checkbox
+    };
+    
+    const handleRoomService = (event) => {
+        setRoomService(event.target.value);  // Convertimos el valor a número
+    };
+    
+    const handleFoodCourt = (event) => {
+        setFoodCourt(event.target.value);  // Convertimos el valor a número
+    };
+    
+    const handleShoppingMall = (event) => {
+        setShoppingMall(event.target.value);  // Convertimos el valor a número
+    };
+    
+    const handleVrDeck = (event) => {
+        setVrDeck(event.target.value);  // Convertimos el valor a número
+    };
+    
+    const handleName = (event) => {
+        setName(event.target.value);
+    };
+    
+    const handleTransported = (event) => {
+        setTransported(event.target.value);  // Asumimos que es un checkbox
+    };
+    
 
     return (
-        <div>
+        <div className="Page">
             <Box className ="Formulario">
-                <Grid2 container spacing={2} columns={2}>
+                <div className="item">
                     <FormControl variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-username">Nombre</InputLabel>
+                        <InputLabel htmlFor="outlined-adornment-username">Passenger ID</InputLabel>
                         <OutlinedInput
                             id="outlined-adornment-username"
                             type="text"
                             label="Username"
-                            onChange={handleUsername}
+                            onChange={handlePassengerId}
                             className="Input"
                         />
                     </FormControl>
-                    <FormControl variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-username">Username</InputLabel>
+                </div>
+                <div className="item">
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Home Planet</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={homePlanet}
+                                label="Age"
+                                onChange={handleHomePlanet}>
+                                <MenuItem value={"Tierra"}>Tierra</MenuItem>
+                                <MenuItem value={"Luna"}>Luna</MenuItem>
+                                <MenuItem value={"Marte"}>Marte</MenuItem>
+                            </Select>
+                    </FormControl>
+                </div>
+                <div className="item">
+                    <FormControlLabel control={<Switch defaultChecked/>} label="Cryo-Sleep" color="warning"/>
+                </div>
+                <div className="item">
+                <FormControl >
+                        <InputLabel id="demo-simple-select-label">Home Planet</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={homePlanet}
+                                label="Age"
+                                onChange={handleHomePlanet}>
+                                <MenuItem value={"a"}>a</MenuItem>
+                                <MenuItem value={"b"}>b</MenuItem>
+                                <MenuItem value={"c"}>c</MenuItem>
+                            </Select>
+                    </FormControl>
+                    <FormControl >
+                        <InputLabel id="demo-simple-select-label">Home Planet</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={homePlanet}
+                                label="Age"
+                                onChange={handleHomePlanet}>
+                                <MenuItem value={"0"}>0</MenuItem>
+                                <MenuItem value={"1"}>1</MenuItem>
+                                <MenuItem value={"2"}>2</MenuItem>
+                            </Select>
+                    </FormControl>
+                    <FormControl >
+                        <InputLabel id="demo-simple-select-label">Home Planet</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={homePlanet}
+                                label="Age"
+                                onChange={handleHomePlanet}>
+                                <MenuItem value={"P"}>P</MenuItem>
+                                <MenuItem value={"S"}>S</MenuItem>
+                            </Select>
+                    </FormControl>
+                </div>
+                <div className="item">
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Destination</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={homePlanet}
+                                label="Age"
+                                onChange={handleHomePlanet}>
+                                <MenuItem value={"Destino 1"}>Destino 1</MenuItem>
+                                <MenuItem value={"Destino 2"}>Destino 2</MenuItem>
+                            </Select>
+                    </FormControl>
+                </div>
+                <div className="item">
+                <h3>Age</h3>
+                <Slider
+                    aria-label="Age"
+                    defaultValue={0}
+                    getAriaValueText={setAge}
+                    valueLabelDisplay="auto"
+                    shiftStep={30}
+                    step={10}
+                    marks
+                    min={10}
+                    max={110}
+                    />
+                </div>
+                <div className="item">
+                    <FormControlLabel control={<Switch defaultChecked/>} label="VIP" color="warning"/>
+                </div>
+                <div className="item">
+                <h3>RoomService</h3>
+                <Slider
+                    aria-label="Age"
+                    defaultValue={0}
+                    getAriaValueText={setAge}
+                    valueLabelDisplay="auto"
+                    shiftStep={30}
+                    step={10}
+                    marks
+                    min={10}
+                    max={110}
+                    />
+                </div>
+                <div className="item">
+                <h3>Food Court</h3>
+                <Slider
+                    aria-label="Age"
+                    defaultValue={0}
+                    getAriaValueText={setAge}
+                    valueLabelDisplay="auto"
+                    shiftStep={30}
+                    step={10}
+                    marks
+                    min={10}
+                    max={110}
+                    />
+                </div>
+                <div className="item">
+                <h3>Shopping-Mall Spa</h3>
+                <Slider
+                    aria-label="Age"
+                    defaultValue={0}
+                    getAriaValueText={setAge}
+                    valueLabelDisplay="auto"
+                    shiftStep={30}
+                    step={10}
+                    marks
+                    min={10}
+                    max={110}
+                    />
+                </div>
+                <div className="item">
+                <h3>VR Deck</h3>
+                <Slider
+                    aria-label="Age"
+                    defaultValue={0}
+                    getAriaValueText={setAge}
+                    valueLabelDisplay="auto"
+                    shiftStep={30}
+                    step={10}
+                    marks
+                    min={10}
+                    max={110}
+                    />
+                </div>
+                <div className="item">
+                <FormControl variant="outlined">
+                        <InputLabel htmlFor="outlined-adornment-username">Name</InputLabel>
                         <OutlinedInput
                             id="outlined-adornment-username"
                             type="text"
                             label="Username"
-                            onChange={handleUsername}
+                            onChange={handleName}
                             className="Input"
                         />
                     </FormControl>
-                    <FormControl variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-username">Username</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-username"
-                            type="text"
-                            label="Username"
-                            onChange={handleUsername}
-                            className="Input"
-                        />
-                    </FormControl>
-                    <FormControl variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-username">Username</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-username"
-                            type="text"
-                            label="Username"
-                            onChange={handleUsername}
-                            className="Input"
-                        />
-                    </FormControl>
-                    <FormControl variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-username">Username</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-username"
-                            type="text"
-                            label="Username"
-                            onChange={handleUsername}
-                            className="Input"
-                        />
-                    </FormControl>
-                    <FormControl variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-username">Username</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-username"
-                            type="text"
-                            label="Username"
-                            onChange={handleUsername}
-                            className="Input"
-                        />
-                    </FormControl>
-                    <FormControl variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-username">Username</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-username"
-                            type="text"
-                            label="Username"
-                            onChange={handleUsername}
-                            className="Input"
-                        />
-                    </FormControl>
-                    <FormControl variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-username">Username</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-username"
-                            type="text"
-                            label="Username"
-                            onChange={handleUsername}
-                            className="Input"
-                        />
-                    </FormControl>
-                    <FormControl variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-username">Username</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-username"
-                            type="text"
-                            label="Username"
-                            onChange={handleUsername}
-                            className="Input"
-                        />
-                    </FormControl>
-                    <FormControl variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-username">Username</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-username"
-                            type="text"
-                            label="Username"
-                            onChange={handleUsername}
-                            className="Input"
-                        />
-                    </FormControl>
-                    <FormControl variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-username">Username</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-username"
-                            type="text"
-                            label="Username"
-                            onChange={handleUsername}
-                            className="Input"
-                        />
-                    </FormControl>
-                    <FormControl variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-username">Username</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-username"
-                            type="text"
-                            label="Username"
-                            onChange={handleUsername}
-                            className="Input"
-                        />
-                    </FormControl>
-                </Grid2>
+                </div>
+                <div className="item">
+                    <FormControlLabel control={<Switch defaultChecked/>} label="Transported" color="warning"/>
+                </div>
             </Box>
         </div>
     );
