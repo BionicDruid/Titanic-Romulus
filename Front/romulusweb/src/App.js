549a1f8109597d8b6939c3aea6f8a2d03a144ed8
Eric <eric_manuel14@hotmail.com>
2024-09-11 17:50:07 -0600

    Ericsin cambios, ya gus, dejame tocar botones :c

--- Front/romulusweb/src/App.js
+++ Front/romulusweb/src/App.js
@@ -1,4 +1,3 @@
-import { Container } from "@mui/material";
 import "./Styles/App.css";
 import Formulario from "./App/Pages/Page";
 import Header from "./App/Pages/Header";
@@ -7,11 +6,10 @@ import Footer from "./App/Pages/Footer";
 function App() {
   return (
     <div className="App">
-      <Container>
+      
         <Header />
         <Formulario />
         <Footer />
-      </Container>
     </div>
   );
 }
