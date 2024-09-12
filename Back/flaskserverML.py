8bda11f6b8b1d8551ece60dc1848dd5d9bb76401
Eric <eric_manuel14@hotmail.com>
2024-09-12 17:53:18 -0600

    Server Flask

--- Back/flaskserverML.py
+++ Back/flaskserverML.py
@@ -1,16 +1,39 @@
 from flask import Flask, request, jsonify
 import pickle
+import numpy as np
 
 app = Flask(__name__)
 
-# Load the model
-with open('finalized_model.sav', 'rb') as model_file:
-    model = pickle.load(model_file)
+
+# Use pickle to load in the pre-trained model
+with open('model.sav', 'rb') as model_file:
+    loaded_model = pickle.load(model_file)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    input_data = request.json['data']
-    prediction = model.predict([input_data])
+    
+    # JSON Request
+    data = request.json
+    
+    # Extract the input data
+    input_data = np.array([
+        data['passengerID'],
+        data['homePlanet'],
+        data['cryoSleep'],
+        data['cabinFinal'],
+        data['destination'],
+        data['age'],
+        data['vip'],
+        data['roomService'],
+        data['foodCourt'],
+        data['shoppingMall'],
+        data['vrDeck']
+    ])  
+    
+    # Make predictions based on the input data
+    prediction = loaded_model.predict([input_data])
+    
+    # Return the prediction as a JSON
     return jsonify({'prediction': prediction[0]})
 
 if __name__ == '__main__':
