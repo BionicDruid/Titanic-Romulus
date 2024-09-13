5f546f3ba51cfa5bfa67cabb7f3affcbe39f7c60
Ericsin <eric_manuel14@hotmail.com>
2024-09-12 21:47:44 -0600

    cors?

--- Back/server.js
+++ Back/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-app.use(express.json()); 
+app.use(cors({
+  origin: '*'
+})); 
 
 
 app.post('/get-data', async (req, res) => {
