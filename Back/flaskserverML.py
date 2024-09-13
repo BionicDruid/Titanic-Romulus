d4d0306a73726325b0d7d518d321e82f80163714
Eric <eric_manuel14@hotmail.com>
2024-09-12 18:06:21 -0600

    Server Flask

--- Back/flaskserverML.py
+++ Back/flaskserverML.py
@@ -9,6 +9,36 @@ app = Flask(__name__)
 with open('finalized_model.sav', 'rb') as model_file:
     loaded_model = pickle.load(model_file)
 
+@app.route('/getPredict', methods=['GET'])
+def predict():
+    # Obtener parámetros desde la URL
+    passengerID = request.args.get('passengerID')
+    homePlanet = request.args.get('homePlanet')
+    cryoSleep = request.args.get('cryoSleep')
+    cabinFinal = request.args.get('cabinFinal')
+    destination = request.args.get('destination')
+    age = request.args.get('age')
+    vip = request.args.get('vip')
+    roomService = request.args.get('roomService')
+    foodCourt = request.args.get('foodCourt')
+    shoppingMall = request.args.get('shoppingMall')
+    vrDeck = request.args.get('vrDeck')
+    
+    # Crear un arreglo con los parámetros recibidos para la predicción
+    input_data = np.array([
+        passengerID, homePlanet, cryoSleep, cabinFinal, destination,
+        age, vip, roomService, foodCourt, shoppingMall, vrDeck
+    ], dtype=object)
+    
+    # Hacer la predicción
+    prediction = loaded_model.predict([input_data])
+    
+    # Convertir la predicción a True/False (por ejemplo, según sea 1 o 0)
+    prediction_bool = bool(prediction[0])
+    
+    # Devolver la predicción como un JSON con True o False
+    return jsonify({'prediction': prediction_bool})
+
 @app.route('/predict', methods=['POST'])
 def predict():
     
